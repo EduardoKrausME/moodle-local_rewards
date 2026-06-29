421bf49e99638f0616b07a68bfefab1a5051f913
Eduardo Kraus <kraus@eduardokraus.com>
2026-06-29 13:48:09 -0300

    V 1.0.2

--- version.php
+++ version.php
@@ -24,8 +24,8 @@
 
 defined('MOODLE_INTERNAL') || die;
 
-$plugin->version = 2026032501;
-$plugin->release = "1.0.1";
+$plugin->version = 20260602900;
+$plugin->release = "1.0.2";
 $plugin->component = "local_rewards";
 $plugin->requires = 2023100900;
 $plugin->maturity = MATURITY_STABLE;
